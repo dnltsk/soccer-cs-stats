6960d9835bba1bb86ab7d012b405cb51984314b0
teeschke <daniel.teske@gmail.com>
2016-03-22 01:40:03 +0100

    initial throw

---
+++ age-vs-value.r
@@ -0,0 +1,29 @@
+#install.packages("RJDBC")
+#install.packages("properties")
+#install.packages("ggplot2")
+library("RJDBC")
+library("properties")
+library("ggplot2")
+
+setwd("D:/Projekte/soccer-cs-stats/")
+
+dbConfig <- read.properties("db-config.properties")
+
+# how-to connect to Heroku Postgres: http://stackoverflow.com/questions/15853167/problems-connecting-remotely-to-postgresql-on-heroku-from-r-using-rpostgresql
+pgsql <- JDBC("org.postgresql.Driver", "postgresql-9.4.1208.jre6.jar", "`")
+testdb <- dbConnect(pgsql, 
+                    paste("jdbc:postgresql://", dbConfig$host, ":", dbConfig$port, "/", dbConfig$base, sep=""), 
+                    user=dbConfig$user,
+                    password=dbConfig$pass,
+                    ssl="true",
+                    sslfactory="org.postgresql.ssl.NonValidatingFactory")
+
+ageValues <- dbGetQuery(testdb, "SELECT  p.id, p.full_name, v.date, (v.date - p.birth_date)/365.0 as age, v.value_in_mio FROM player p, player_market_value v where p.id = v.id_player order by p.id, v.date")
+
+str(ageValues)
+
+x <- head(ageValues, n=50)
+
+ggplot(ageValues, aes(age, value_in_mio, group=id, color=full_name)) +
+  geom_line() +
+  theme(legend.position="none")

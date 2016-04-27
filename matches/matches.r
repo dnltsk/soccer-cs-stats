3a36bf40c065a8cdce6197098a699880dc6ef6d0
Daniel Teske <daniel.teske@meteogroup.com>
2016-04-28 00:06:45 +0200

    correlation plot created

--- matches/matches.r
+++ matches/matches.r
@@ -2,69 +2,47 @@ require("properties")
 require("RPostgreSQL")
 require("ggplot2")
 require("Cairo")
+require("corrplot")
 
-setwd("D:/Projekte/soccer-cs-stats/market-value")
+setwd("/projects/soccer-cs-stats/matches")
 
 dbConfig <- read.properties("../db-config.properties")
 
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, dbname=dbConfig$dbname,host=dbConfig$host,port=dbConfig$port,user=dbConfig$user,password=dbConfig$pass) 
 
-ageValues <- dbGetQuery(con, "SELECT  p.id, p.full_name, p.position_vertical, v.date, (v.date - p.birth_date)/365.0 as age, v.value_in_mio FROM player p, player_market_value v where p.id = v.id_player order by p.id, v.date")
+matches <- dbGetQuery(con, 
+              "with x as (
+              	select m.id, m.date, m.home, m.guest, home_goals, guest_goals,
+                            p.full_name, p.position_vertical, p.nationality,
+                            (select value_in_mio from player_market_value v where v.id_player = p.id and v.date <= m.date order by v.date desc limit 1) as current_value
+                            from match m, player_in_match pm, player p
+                            where --m.date < '2011-01-01' and
+                            m.id = pm.id_match
+                            and p.id = pm.id_player
+              )
+              select x.id, x.date, x.home, x.guest, x.home_goals, x.guest_goals,
+              (select COALESCE(sum(current_value), 0) from x as x2 where x.id = x2.id and x.home = x2.nationality and x2.position_vertical = 'KEEPER') as g_keeper_value,
+              (select COALESCE(sum(current_value), 0) from x as x2 where x.id = x2.id and x.home = x2.nationality and x2.position_vertical = 'DEFENSE') as g_defense_value,
+              (select COALESCE(sum(current_value), 0) from x as x2 where x.id = x2.id and x.home = x2.nationality and x2.position_vertical = 'MIDFIELD') as g_midfield_value,
+              (select COALESCE(sum(current_value), 0) from x as x2 where x.id = x2.id and x.home = x2.nationality and x2.position_vertical = 'OFFENSE') as g_offense_value,
+              (select COALESCE(sum(current_value), 0) from x as x2 where x.id = x2.id and x.guest = x2.nationality and x2.position_vertical = 'KEEPER') as h_keeper_value,
+              (select COALESCE(sum(current_value), 0) from x as x2 where x.id = x2.id and x.guest = x2.nationality and x2.position_vertical = 'DEFENSE') as h_defense_value,
+              (select COALESCE(sum(current_value), 0) from x as x2 where x.id = x2.id and x.guest = x2.nationality and x2.position_vertical = 'MIDFIELD') as h_midfield_value,
+              (select COALESCE(sum(current_value), 0) from x as x2 where x.id = x2.id and x.guest = x2.nationality and x2.position_vertical = 'OFFENSE') as h_offense_value
+              from x
+              group by id, date, home, guest, home_goals, guest_goals")
 
-a1 <- ggplot(ageValues, aes(age, value_in_mio, group=id, color=position_vertical)) +
-  geom_line() +
-  theme(legend.position="bottom") +
-  ggtitle("Player's age vs. Player's market value")
-Cairo(file="year-vs-value.png", 
-      bg="white",
-      type="png",
-      units="in", 
-      width=600/72, 
-      height=600/72, 
-      dpi=72)
-a1
-dev.off()
+names(matches)
+M <- cor(matches[, c("home_goals", "guest_goals",
+                     "h_keeper_value", "h_defense_value", "h_midfield_value", "h_offense_value",
+                     "g_keeper_value", "g_defense_value", "g_midfield_value", "g_offense_value")])
 
-a2 <- ggplot(ageValues, aes(as.Date(date), value_in_mio, group=id, color=position_vertical)) +
-  geom_line() +
-  scale_x_date() +
-  theme(legend.position="bottom") +
-  ggtitle("Date vs. Player's market value")
-Cairo(file="year-vs-value.png", 
-      bg="white",
-      type="png",
-      units="in", 
-      width=1000/72, 
-      height=500/72, 
-      dpi=72)
-a2
-dev.off()
-
-
-yearValues <- dbGetQuery(con, "SELECT  p.id, p.full_name, p.position_vertical, EXTRACT(YEAR FROM v.date) as year, max(v.value_in_mio) as value_in_mio
-                               FROM player p, player_market_value v
-                               WHERE p.id = v.id_player 
-                               GROUP BY p.id, p.full_name, p.position_vertical, year")
-
-b <- ggplot(yearValues, aes(year, value_in_mio, group=interaction(year, position_vertical), colour=position_vertical)) +
-  geom_boxplot(position=position_dodge(width=0.9), width=1) +
-  stat_summary(fun.y=median, geom="line", position=position_dodge(width=0.9), 
-               aes(group=position_vertical)) +
-  stat_summary(fun.y=median, geom="point", size=2,  position=position_dodge(width=0.9), 
-               aes(group=position_vertical)) +
-  scale_x_continuous(breaks=unique(yearValues$year)) +
-  theme(legend.position="bottom") +
-  ggtitle("median market value by year and position")
-
-Cairo(file="boxplot.png", 
-      bg="white",
-      type="png",
-      units="in", 
-      width=1000/72, 
-      height=500/72, 
-      dpi=72)
-b
+png(height=600, width=600, file="corrplot.png")
+corrplot(M, 
+         method="circle", 
+         mar=c(0,0,2,0),
+         title="correlation plot of GOALS vs. aggregated MARKET VALUES on position")
 dev.off()
 
 #cleanup
